f0c80d40e32668a73bea740c95e20db36736a741
murat türkgeldi <loopus3@gmail.com>
2025-12-20 13:54:56 +0000

    Tamam mayk, artık tablo çok net. Şu an “Kaydet” akışında 3 ayrı problem

--- src/app/orders/[id]/edit/page.tsx
+++ src/app/orders/[id]/edit/page.tsx
@@ -1,12 +1,10 @@
-
-
 import EditOrderClient from './EditOrderClient';
 
 type PageProps = {
   params: { id: string };
 };
 
-export default function EditOrderPage({ params }: PageProps) {
+export default async function EditOrderPage({ params }: PageProps) {
   const orderId = params.id;
   return <EditOrderClient orderId={orderId} />;
 }
