ab03e584b5d964acfc23c083199980e251522640
murat türkgeldi <loopus3@gmail.com>
2025-12-27 15:23:04 +0000

    Kanka bu button.tsx zaten doğru: asChild prop’unu destructure edip DOM’a

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -553,7 +553,7 @@ const SidebarMenuButton = React.forwardRef<
     },
     ref
   ) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp: any = asChild ? Slot : "button"
     const { isMobile, state } = useSidebar()
 
     const button = (
